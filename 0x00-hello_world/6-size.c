5dfd60a9eba608029b105651ebd6af413014298a
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-08 00:49:03 +0100

    Updated format

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of a char:", sizeof(char));
-printf("Size of a int:", sizeof(int));
-printf("Size of a long int:", sizeof(long int));
-printf("Size of a long long int:", sizeof(long long int));
-printf("Size of a float:", sizeof(float));
+printf("Size of a char: %c", sizeof(char));
+printf("Size of a int: %d", sizeof(int));
+printf("Size of a long int: %li", sizeof(long int));
+printf("Size of a long long int: %lli", sizeof(long long int));
+printf("Size of a float: %d", sizeof(float));
 return (0)
 } 
