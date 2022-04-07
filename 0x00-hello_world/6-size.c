e2568a4bc6fa8d1cb268d1aebc042eba7a60b067
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-08 00:54:10 +0100

    Corrected the error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-printf("Size of a char: %c", sizeof(char));
-printf("Size of a int: %d", sizeof(int));
-printf("Size of a long int: %d", sizeof(long int));
-printf("Size of a long long int: %d", sizeof(long long int));
-printf("Size of a float: %f", sizeof(float));
-return (0)
+printf("Size of a char: %lu", sizeof(char));
+printf("Size of a int: %lu", sizeof(int));
+printf("Size of a long int: %lu", sizeof(long int));
+printf("Size of a long long int: %lu", sizeof(long long int));
+printf("Size of a float: %lu", sizeof(float));
+return (0);
 } 
