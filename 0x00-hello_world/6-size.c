1f0583e5607d1c9f82c9fb0f8f899e833d321f63
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-08 00:40:03 +0100

    Size of some dtype

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*
+* main - Function begins
+*
+* Return: end of function and return o
+*/
+
+int main(void)
+{
+printf("Size of a char:", sizeof(char));
+printf("Size of a int:", sizeof(int));
+printf("Size of a long int:", sizeof(long int));
+printf("Size of a long long int:", sizeof(long long int));
+printf("Size of a float:", sizeof(float));
+return (0)
+} 
