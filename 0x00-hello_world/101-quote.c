718d6ab2d121100a8448a75eab00921f2583813d
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-08 01:19:08 +0100

    Print without stdio.h

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+/**
+* main - Start of function
+*
+* Return - returns 1 when successful
+*/
+
+int main(void)
+{
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1)
+}
