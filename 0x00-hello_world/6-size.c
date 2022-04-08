7c8790040020c5073964de08aabedee9e97cf2ec
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-08 01:06:39 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-/*
+/**
 * main - Entry point
 *
 * Return: Always 0 (Success)
@@ -12,6 +12,6 @@ printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-printf("Size of a float: %lu byte(s)", sizeof(float));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
