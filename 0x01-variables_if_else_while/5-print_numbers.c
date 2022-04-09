1f759ccfccab68883f2ec0db43907f4777d3df04
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-09 02:21:40 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Starting point
+* prints all digits
+* Return: return 0 if successful
+*/
+
+int main(void)
+{
+
+int num;
+
+for (num=0; num<10, num++)
+{
+putchar(num);
+}
+putchar('\n');
+
+return(0);
+}
