882a6c38ab55e4380d1d8446e2740565c86fb0dc
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-09 03:59:01 +0100

    Special printing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Start of function
+* prints all possible combo of numbers seperated *withcomma
+* Return: returns 1 when successful
+*/
+
+int main(void)
+{
+
+int c;
+int d;
+for (c = 48; c <= 57; c++)
+{
+
+for (d = c+1; d <= 57; d++)
+{
+putchar(c);
+putchar(d);
+if (c < 56)
+{
+putchar(44);
+putchar(' ');
+}
+
+}
+}
+putchar('\n');
+return (0);
+
+
+}
