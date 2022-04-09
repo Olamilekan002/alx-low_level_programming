c8926642932eae4f64a48edd91ac128fe9389ef8
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-09 04:07:22 +0100

    3 num combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - Start of function
+* prints combo of 3 numbers 1-9
+* Return: returns 0 when successful
+*/
+
+int main(void)
+{
+
+int x;
+int y;
+int z;
+
+for (x = 48; x <= 57; x++)
+{
+
+for (y = x + 1; y <= 57; y++)
+{
+
+for (z = y + 1; z <= 57; z++)
+{
+putchar(x);
+putchar(y);
+putchar(z);
+
+if (x < 56)
+{
+putchar(44);
+putchar(' ');
+}
+
+}
+}
+}
+putchar('\n');
+return (0);
+
+
+}
