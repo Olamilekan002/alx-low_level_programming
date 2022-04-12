1c8658f4b3979b9bc38e86617de741478afd7e37
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-12 23:49:39 +0100

    Create 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* main - Entry function
+*
+* Return: Return 0 if successful
+*/
+
+int main(void)
+{
+void print_alphabet()
+    return (0);
+}
+
+void print_alphabet()
+{
+for(int i = 97; i<=122; i++)
+{
+_putchar('i');
+}
+_putchar('\n');
+}
