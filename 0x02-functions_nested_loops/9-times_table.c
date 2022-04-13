1e006d7e75619d1a7a7eeacf8fc6393c447107dd
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-13 01:56:25 +0100

    prints multiplication table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints time
+*
+* Return : no return
+*/
+
+void jack_bauer(void)
+{
+int i, j, mul;
+
+for (i = 0; i <= 9; i++)
+{
+
+for (j = 0; j <= 9; j++)
+{
+mul = i * j;
+
+_putchar (mul / 10 + '0');
+_putchar (mul % 10 + '0');
+if (j != 9)
+{
+_putchar (',');
+_putchar (' ');
+}
+}
+_putchar ('\n');
+
+}
+
+}
