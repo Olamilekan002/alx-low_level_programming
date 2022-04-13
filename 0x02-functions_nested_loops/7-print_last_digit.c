0d88e865121fe768b41f25ce06c2d5702e96fc4d
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-13 01:17:30 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,18 @@ int print_last_digit(int n)
 {
 int last;
 
-last = n % 10;
+if (n < 0)
+{
+last = (-1 * (n % 10));
 _putchar(last + '0');
 return (last);
+}
+  
+else
+{
+last = n % 10;
+_putchar(last);
+return (last);
+}
+
 }
