ba420fce3949c8fe153252f4462afe8b912310eb
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-13 03:34:09 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,8 +14,8 @@ if (n >= 98)
 for (n = n; n > 98; n--)
 {
 printf("%d, ", n);
-printf("%d\n", 98);
 }
+printf("%d\n", 98);
 }
 
 else
@@ -23,7 +23,7 @@ else
 for (n = n; n < 98; n++)
 {
 printf("%d, ", n);
-printf("%d\n", 98);
 }
+printf("%d\n", 98);
 }
 }
