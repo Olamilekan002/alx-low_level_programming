b414f65337f9efd87e2a41d80ad3f8fe211ecc37
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-13 01:32:28 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 {
 int i, j, k, l;
 
-for ( i = 0; i < 3; i++)
+for (i = 0; i < 3; i++)
 {
 for (j = 0; j < 4; j++)
 {
@@ -20,8 +20,10 @@ for (l = 0; l <= 9; l++)
 {
 _putchar(i + '0');
 _putchar(j + '0');
+_putchar(':');
 _putchar(k + '0');
 _putchar(l + '0');
+_putchar('\n');
 }
 }
 }
