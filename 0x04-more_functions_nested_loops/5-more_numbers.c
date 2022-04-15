7a6c7900558308399f7101d1c1fce5f68cdaab72
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-15 02:26:28 +0100

    prints 1-14 10times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 1-14 * 10
+ */
+
+void more_numbers(void)
+{
+int x, y;
+
+for (x = 0; x < 11; x++)
+{
+for (y = 0; y < 15; y++)
+{
+_putchar(y + '0');
+}
+_putchar('\n');
+}
+_putchar('\n');
+}
