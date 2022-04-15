acc82f9b53e26df5650f34fbf33b59888f8a7b4c
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-15 02:56:34 +0100

    Create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_line - prints  straight line
+* @n: lenght of line
+*/
+
+void print_line(int n)
+{
+if (n > 0)
+{
+for (n = n; n >= 0; n--)
+{
+_putchar('_');
+}
+}
+else
+{
+_putchar('\n')
+}
+}
