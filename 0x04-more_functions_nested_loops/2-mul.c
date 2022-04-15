e72db384a8b40ebff8a4c882f4c8293401697bda
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-15 01:44:53 +0100

    gets product of two num

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include 'main.h'
+#include <stdio.h>
+
+/**
+* mul - multiply two numbers
+* @num1: first number
+* @num2: second number
+*
+* Return: Returns the product of the two num
+*/
+
+int mul(int num1, int num2)
+{
+return ( num1 * num2);
+}
