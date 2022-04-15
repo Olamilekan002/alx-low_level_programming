f8f7d090d5a70f46ad9668da4bb1fd9d7157ae4b
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-15 02:16:15 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,14 @@
 
 /**
 * print_most_numbers - print numbers except 2 & 4
-*
 */
+
 void print_most_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
 {
-if (i != 2 || i != 4)
+if (i != 2 && i != 4)
 {
 _putchar(i + '0');
 }
