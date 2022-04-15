ffb56eda6178c9593193287c5f84dc6aeae39619
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-15 05:02:27 +0100

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - draws a straight line in the terminal
+*
+*/
+void main()
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if ((i % 3 == 0) && (i % 5 == 0))
+{
+printf("FizzBuzz ");
+}
+
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+
+else
+{
+printf("%d ",(i));
+}  
+}
+}
