7920f60d624d2b48573e04ed0fad4f2489bc905c
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 05:04:29 +0100

    print a number of elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_array - prints a number given of elements
+*
+*@a: the array
+*@n: number of elements 
+*/
+
+void print_array(int *a, int n)
+{
+int length, i;
+
+for (length = 0; a[length] != '\0'; length++)
+{
+}
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+printf("\n");
+}
