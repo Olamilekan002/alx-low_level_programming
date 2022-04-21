e66567c5a0b79f2ae4ea9ea88fdf964a6bb02451
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 05:16:55 +0100

    Create 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcpy - prints a number given of elements
+*
+*@dest: destination array
+*@src: source array
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+int length, i;
+
+for (length = 0; a[length] != '\0'; length++)
+{
+}
+
+for (i = 0; i <= length; i++)
+{
+dest[i] = src[i];
+}
+return (dest);
+}
