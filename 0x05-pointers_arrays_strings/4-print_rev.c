9fde95b977d240b710e115990f185e4306f17cd5
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 03:01:01 +0100

    reverse a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev - reverses string and to stdout.
+*
+* @s: the string to be printed to stdout
+*/
+
+void print_rev(char *s);
+{
+int len;
+
+len = _strlen(s); /* a funtion that was once written */
+
+while (len >= 0)
+{
+_putchar(*(s + len));
+len--;
+}
+
+_putchar('\n');
+}
