c47b0919de6aeb956f37a6fed86ad94e9cc8abf6
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 02:19:40 +0100

    counts the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string
+*
+* @s: the string to be counted
+* Return: returns length of string
+*/
+
+int _strlen(char *s)
+{
+int len;
+
+len = 0;
+while(*s != '\0')
+{
+len++;
+*s++;
+}
+return len;
+}
