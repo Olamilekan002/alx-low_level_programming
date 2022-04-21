b4bda84c443b9664876fe53ec322522f06d93f57
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 04:35:06 +0100

    prints varying strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other string.
+ *
+ *@str: the string to be reversed.
+ */
+
+void puts2(char *str)
+{
+int length, i;
+
+for (length = 0; s[length] != '\0'; length++)
+{
+}
+
+i = length - 1;
+for (i = 0; i < length; i += 2)
+{
+_putchar(str[i]);
+}
+}
