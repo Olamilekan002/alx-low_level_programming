0ef50f1f264858ae6cbaa24ef162208fe50b0e4d
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-21 02:48:11 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,15 @@
 #include "main.h"
- /**
- * _puts - prints string to stdout
- *
- * @str: the string to be printed to stdout
- */
+
+/**
+* _puts - prints string to stdout.
+*
+* @str: the string to be printed to stdout
+*/
 
 void _puts(char *str)
 {
 
-while(*str != '\0')
+while (*str != '\0')
 {
 _putchar(*str);
 str++;
