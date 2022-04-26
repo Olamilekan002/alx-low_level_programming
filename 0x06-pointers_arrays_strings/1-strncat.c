1fe2ea711c91c3a7a5becd9f212f17c5d8f9152e
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-26 04:56:57 +0100

    ⁷

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,21 @@
 #include "main.h"
-
-/**
-*_strncat - concatenate two string
+/*
 *
-*@dest: destination string
-*@src: source string
-*@n: limit from src
-*
-*Return: returns the pointer to dest
+*_strncat - concatinate 2 strings.
+*@dest: first string.
+*@src: second string.
+*@n: the number of bytes to use from src.
+*Return: string.
 */
+
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, j = 0;
+int i = 0, k = 0;
 
 while (dest[i] != '\0')
-{
 i++;
-}
 
-while (src[k] != '\0' && n > k)
+while (src[k] != '\0' && n > k)	
 {
 dest[i] = src[k];
 k++;
