387eca70c3f4698ad8e163686bf35acb4cec7193
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-27 04:32:02 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*_strchr - Finds a char in a string
+*
+*@s: the memory
+*@c: constant byte
+*Return: points to the c
+*/
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+i++;
+}
+
+return (NULL);
+}
