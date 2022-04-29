dace45319c4416fe25ad23f0c37f32a41b8b379c
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-29 02:24:19 +0100

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,13 @@
 
 /**
 * _print_rev_recursion - reverses a string
-* @s: input string to be reversed 
-* Return: 
+* @s: input string to be reversed
+* Return: void
 */
 
 void _print_rev_recursion(char *s)
 {
-if (s != 0)
+if (*s != 0)
 {
 _print_rev_recursion(s + 1);
 _putchar(*s);
