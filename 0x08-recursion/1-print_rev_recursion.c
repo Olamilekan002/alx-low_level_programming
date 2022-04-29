f7f45c1d6f3a39df269c8bbf27ba4dd52dc2c78e
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-29 02:21:18 +0100

    Reverses a string recursively

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - reverses a string
+* @s: input string to be reversed 
+* Return: 
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (s != 0)
+{
+_puts_recursion(s + 1);
+_putchar(*s);
+}
+}
