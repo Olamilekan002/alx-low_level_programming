c0ab11cdb11a5a34cc994164ed92371edfc48545
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-29 03:09:49 +0100

    Calculates factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* factorial - gets the factorial of a num
+* @n: input number
+* Return: int
+*/
+
+int factorial(int n)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{ return (1);
+}
+if (n >= 1)
+{
+return (n * factorial(n-1));
+}
+}
