5acceba3bdbc11eb62f00d076242ee2e50627fa8
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-04-29 03:11:29 +0100

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
 * factorial - gets the factorial of a num
@@ -8,15 +8,20 @@
 
 int factorial(int n)
 {
+
 if (n < 0)
 {
 return (-1);
 }
+
 if (n == 0)
-{ return (1);
+{
+return (1);
 }
+
 if (n >= 1)
 {
 return (n * factorial(n-1));
 }
+
 }
