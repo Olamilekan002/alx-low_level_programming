05a6616cdb4eaab166d58acc214b600b311570d7
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-04 00:52:00 +0100

    Argv and argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - start
+ *
+ * @argc: Counts the number of parameters that go into main
+ * @argv: Pointer of array of pointers containing strings entering main
+ * Return: 0 when successful 
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+	return (0);
+}
