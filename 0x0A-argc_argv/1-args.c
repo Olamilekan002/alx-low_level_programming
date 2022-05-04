f499853e7a17edd939636c078826ff5e57c08212
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-04 01:26:37 +0100

    Check for number of arguments entered

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - start
+*
+* @argc: Counts the number of parameters that go into main
+* @argv: Pointer of array of pointers containing strings entering main
+* Return: 0 when successful
+*/
+
+int main(int argc, char **argv)
+{
+printf("%s\n", argc);
+return (0);
+}
