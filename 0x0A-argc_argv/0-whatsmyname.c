9b12c46389609feeaaa85ef1ce58286591c45129
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-04 01:22:14 +0100

    Betty error fixed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * main - start
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: 0 when successful 
- */
+* main - start
+*
+* @argc: Counts the number of parameters that go into main
+* @argv: Pointer of array of pointers containing strings entering main
+* Return: 0 when successful
+*/
 
 int main(int argc, char **argv)
 {
