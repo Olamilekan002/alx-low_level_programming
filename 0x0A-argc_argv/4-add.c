0a1abfed223e5b39c4dd1622bcafc799771f50c7
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-04 19:26:00 +0100

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - start
+*
+* @argc: Counts the number of parameters that go into main
+* @argv: Pointer of array of pointers containing strings entering main
+* Return: 1 when failed
+*/
+
+int main(int argc, char **argv)
+{
+int mul, x;
+
+x = 0;
+sum = 0;
+if (argc < 2)
+{
+mul = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", mul);
+}
+else
+{
+x = 1;
+printf("%s\n", "Error");
+}
+
+return (x);
+}
