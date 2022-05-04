dc8268fa492273764a311cf5f8a82287d967097e
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-04 02:07:38 +0100

    Multiples two arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - start
+*
+* @argc: Counts the number of parameters that go into main
+* @argv: Pointer of array of pointers containing strings entering main
+* Return: 1 when successful
+*/
+
+int main(int argc, char **argv)
+int mul;
+{
+
+if (argc == 3)
+{
+mul = argv[1] * argv[2];
+printf("%d\n", mul);
+}
+else
+{
+printf("Error\n");
+}
+return (1);
+}
