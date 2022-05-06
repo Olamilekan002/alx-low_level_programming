2da6183b7f794da6ad9a6d19b6e18b0d7e8081dc
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-06 04:11:12 +0100

    debugging

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,30 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
-	char *str;
+int i, j, k;
+char *str;
 
-	if (s1 == NULL)
-		s1 = "";
-  if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	for (i = 0; s1[i]; i++)
-	;
-  for (j = 0; s2[j]; j++)
-	;
-	j++;
-    
-	str = malloc(sizeof(char) * (i+j));
+for (i = 0; s1[i]; i++)
+;
+for (j = 0; s2[j]; j++)
+;
+j++;
+
+str = malloc(sizeof(char) * (i+j));
   
-	for (k = 0; k < i; k++)
-	{
-		str[k] = s1[k];
-	}
-  for (; k < i + j; k++)
-  {
-		str[i + j] = s2[k - 1];
+for (k = 0; k < i; k++)
+{
+str[k] = s1[k];
+}
+
+for (; k < i + j; k++)
+{
+		str[i + j] = s2[k - i];
   }
 	str[k] = '\0';
 	return (str);
