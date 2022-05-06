04f193c05c732f52e0a7f37028f9c168728ddc07
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-06 01:51:55 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *ptr;
-int i;
+unsigned int i;
 
 ptr = malloc(sizeof(char) * size);
 
@@ -22,5 +22,5 @@ for (i = 0; i < size; i++)
 ptr[i] = c;
 }
 
-return (ptr);n");
+return (ptr);
 }
