ef4e71c45495b09fddbe0073e60f9d882715e5dd
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-06 04:35:20 +0100

    Creating a matrix

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *alloc_grid - a matrix.
+ *@width: the column.
+ *@height: the row
+ *Return: pointer to the concatenated string if success.
+*/
+
+int **alloc_grid(int width, int height)
+{
+int *mat;
+int k;
+
+if (width < 1 || height < 1)
+{
+return (NULL);
+}
+
+matr = malloc(sizeof(*mat) * (width * height));
+if (mat == NULL)
+{
+return (NULL);
+}
+
+for (k = 0; k < (width * height); k++)
+{
+mat[k] = 0;
+}
+
+return (mat);
+}
