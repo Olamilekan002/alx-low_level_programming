43f3138a564b6e0e781ff4c393eac9f1df7837ac
Olamilekan <omotoshomatthew73@gmail.com>
2022-05-06 04:48:36 +0100

    error debugged

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,33 +1,47 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *alloc_grid - a matrix.
- *@width: the column.
- *@height: the row
- *Return: pointer to the concatenated string if success.
-*/
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
+ *
+ * Return: Double Pointer
+ */
 
 int **alloc_grid(int width, int height)
 {
-int *mat;
-int k;
+	int **s;
+	int i, j, k, fr;
 
-if (width < 1 || height < 1)
-{
-return (NULL);
-}
+	fr = 0;
+	if (width <= 0 || height <= 0)
+		return (0);
 
-mat = malloc(sizeof(*mat) * (width * height));
-if (mat == NULL)
-{
-return (NULL);
-}
-
-for (k = 0; k < (width * height); k++)
-{
-mat[k] = 0;
-}
-
-return (0);
+	s = malloc(height * sizeof(int *));
+	if (s == 0)
+		return (0);
+	for (i = 0; i < height; i++)
+	{
+		*(s + i) = malloc(width * sizeof(int));
+		if (*(s + i) == 0)
+		{
+			fr = 1;
+			break;
+		}
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	if (fr == 1)
+	{
+		for (k = 0; k <= i; k++)
+		{
+			free(*(s + k));
+		}
+		free(s);
+	}
+	return (s);
 }
