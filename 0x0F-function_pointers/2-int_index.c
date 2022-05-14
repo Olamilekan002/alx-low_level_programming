00d3f7789e8b6d578ef738491b9f900a801f6a37
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-14 05:20:50 +0100

    Betty error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 if (cmp && array)
-	{
-		for (i = 0; i < size; ++i)
-		{
-			if (cmp(array[i]))
-				return (i);
-		}
-	}
+{
+for (i = 0; i < size; ++i)
+{
+if (cmp(array[i]))
+return (i);
+}
+}
 
-	return (-1);
+return (-1);
 }
