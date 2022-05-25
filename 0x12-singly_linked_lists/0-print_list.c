884556c068012f01d3be39203c7c0e6180d0949e
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-24 18:06:52 -0700

    betty error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * @h: the list to print
  *
  * Description: Print each list item, prefixed by it's length, formatted using
- * "[%d] %s\n". If a list item is NULL, print it as "(nil)" with zero length.
+ * "[%d] %s\n". If a list item is NULL, print it as "(nil)" with length zero.
  *
  * Return: size of the list
  */
