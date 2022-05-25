05d36f26d9e4fe067fa3aa53fc557d7ebe862169
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-24 17:33:56 -0700

    prints all element of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - print list items
+ * @h: the list to print
+ *
+ * Description: Print each list item, prefixed by it's length, formatted using
+ * "[%d] %s\n". If a list item is NULL, print [0] (nil).
+ *
+ * Return: size of the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	if (h)
+	{
+		if (h->str)
+			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		return (print_list(h->next) + 1);
+	}
+	return (0);
+}
