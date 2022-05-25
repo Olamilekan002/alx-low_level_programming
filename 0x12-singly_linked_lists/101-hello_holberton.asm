f1d640a07e0f75f385f7a1b7c90a878498d7a210
Olamilekan Omotosho <58680291+Olamilekan002@users.noreply.github.com>
2022-05-24 20:29:24 -0700

    print in assembly lang

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+/* a 64-bit program in assembly that prints Hello, Holberton, followed by a new line.*/
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
+	extern printf
+	global main
+main:
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
+
+	mov eax, 0
+	ret
