84255e17214b011750ec9d92e2951d4c124e343f
Olamilekan <omotoshomatthew73@gmail.com>
2022-05-27 02:51:17 +0100

    task3

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,18 +10,18 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *newnode;
 
 	if (!head)
 		return (NULL);
 
-	new = malloc(sizeof(listint_t));
+	newnode = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
 
-	new->n = n;
-	new->next = *head;
-	*head = new;
+	newnode->n = n;
+	newnode->next = *head;
+	*head = newnode;
 
-	return (new);
+	return (newnode);
 }
